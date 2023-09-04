344dddfcb7b1fd578f40396de58b066fc08aa926
Sneha S <snehabhaise711@gmail.com>
2023-09-04 15:32:42 +0530

    birthdayprojext

---
+++ script.js
@@ -0,0 +1,31 @@
+const dateOfBirth = document.querySelector("#date-of-birth");
+const luckyNumber = document.querySelector("#lucky-number");
+
+const checkNumberButton = document.querySelector("#check-number");
+const outputBox = document.querySelector("#output-box");
+function compareValues(sum,uickyNumber){
+    if(sum%luckyNumber ===0){
+        outputBox.value="Your Birthday is Lucky🥳";
+    }
+    else{
+        outputBox.value="Oops!! your birthday is nit lucky😔"; 
+    }
+
+}
+function checkNumberIsLucky(){
+    const dob = dateOfBirth.value;
+    const sum = calculateSum(dob);
+    if(sum&&dob)
+    compareValues(sum,luckyNumber.value);
+    else
+    outputBox.value="Please add both the fields😏!!!"; 
+}
+function calculateSum(dob){
+    dob = dob.replaceAll("-","");
+    let sum =0;
+     for(let index=0; index<dob.length;index++){
+        sum = sum + Number(dob.charAt(index));
+     }
+return sum;
+}
+checkNumberButton.addEventListener('click',checkNumberIsLucky)
